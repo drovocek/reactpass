42987d4d1d9f5070aec2fa897b1aac6c68b00dc7
drovocek <mycodepg@gmail.com>
2021-06-04 08:56:00 +0300

    add icon render

--- frontend/views/users/user-view.ts
+++ frontend/views/users/user-view.ts
@@ -3,17 +3,22 @@ import "@vaadin/vaadin-text-field";
 import "@vaadin/vaadin-button";
 import "@vaadin/vaadin-grid";
 import "@vaadin/vaadin-grid/src/vaadin-grid-column";
-import './user-form-view';
 import "@vaadin/vaadin-notification";
+import "@vaadin/vaadin-icons";
+import './user-form-view';
 import {userFilterStore} from "Frontend/views/users/user-filter-store";
 import {GeneralRootView} from "Frontend/views/general/general-root-view";
 import User from "Frontend/generated/ru/volkov/getpass/data/entity/User";
+import {until} from "lit-html/directives/until";
+import {repeat} from "lit-html/directives/repeat";
+
 
 @customElement('user-view')
 export class UserView extends GeneralRootView<User> {
 
     constructor() {
         super(userFilterStore);
+        // this.renderIcons();
     }
 
     //html
@@ -25,10 +30,12 @@ export class UserView extends GeneralRootView<User> {
                .items="${userFilterStore.filtered}"
                .selectedItems="${[userFilterStore.getSelected()]}"
                @active-item-changed="${this.handleGridSelection}"
-             >
+               theme="row-stripes" 
+               column-reordering-allowed 
+               multi-sort>
                    <vaadin-grid-column path="id" auto-width>
                      </vaadin-grid-column>
-                     <vaadin-grid-column path="role" auto-width>
+                     <vaadin-grid-column path="role.name" auto-width>
                      </vaadin-grid-column>
                      <vaadin-grid-column path="fullName" auto-width>
                      </vaadin-grid-column>
@@ -39,6 +46,8 @@ export class UserView extends GeneralRootView<User> {
                      <vaadin-grid-column path="phone" auto-width>
                      </vaadin-grid-column>
                      <vaadin-grid-column path="enabled" auto-width>
+                         <iron-icon icon=@enabled?"vaadin:check-square-o":"vaadin:square-shadow"></iron-icon> 
+                     </template>
                      </vaadin-grid-column>
                      <vaadin-grid-column path="regDate" auto-width>
                      </vaadin-grid-column>
@@ -52,4 +61,16 @@ export class UserView extends GeneralRootView<User> {
            </div>
          `;
     }
+
+    // renderIcons(){
+    //     customElements.whenDefined('vaadin-grid').then(function() {
+    //         const columns = document.querySelectorAll('vaadin-grid-column');
+    //         columns[3].renderer = function(root, column, model) {
+    //             let wrapper = root.firstElementChild;
+    //             if (!wrapper) {
+    //                 root.innerHTML =
+    //                     '<iron-icon icon=@enabled?"vaadin:check-square-o":"vaadin:square-shadow"></iron-icon>';
+    //         };
+    //     }});
+    // }
 }
